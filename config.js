61066dcb9a8136382ef17a89dae116a8eb552838
Bùi Công Tuấn <85726751+tunld23@users.noreply.github.com>
2021-07-20 16:22:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Chào Ngọc nor!',
+    introTitle: 'Chào linh tran dut !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
